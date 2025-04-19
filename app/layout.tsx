fbd0b216022c53d6efda484c686f5dadbd00bead
joseph asmare <160507197+Yosefasmare@users.noreply.github.com>
2025-04-19 10:56:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 
 
   return (
-    <html lang="en">
+    <html lang="en" className=" ">
       <body  className="bg-gray-100 dark:bg-gray-900">
         <ThemeProvider>
            {children}
