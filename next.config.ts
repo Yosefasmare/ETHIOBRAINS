b5945511d64cd4c29a51162d66479ecb45d07fad
joseph asmare <160507197+Yosefasmare@users.noreply.github.com>
2025-04-19 11:03:43 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,13 @@
-import type { NextConfig } from "next";
+export default nextConfig;
+
+add it in here import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none';",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer-when-downgrade",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
+
+
